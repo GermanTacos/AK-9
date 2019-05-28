baa667a14f47582576b2a976df4a48c25eef8651
IKillerwolfI <killerwolf.no1@gmail.com>
2019-05-28 02:13:25 +0200

    Forbids for custom AK sights

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -260,6 +260,13 @@ if attach_tables then
 			table.insert(all_ak9sight, o_id)
 		end
 	end
+	if attach_tables.Custom_AK then --safety (since that thing doesn't exist in older versions)
+		for _, o_id in pairs(attach_tables.Custom_AK) do
+			if self.parts[o_id] then
+				table.insert(self.parts.wpn_fps_upg_o_ak9_l_scopemount.forbids, o_id)
+			end
+		end
+	end
 end
 
 if self.wpn_fps_ass_heffy_762 then
